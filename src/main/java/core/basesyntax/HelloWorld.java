f90d124660e3f0096038f9f108c4edb784a62187
Oleksandr Babenko <babenk00sasha@gmail.com>
2025-06-10 20:02:05 +0200

    changed greeting to salute

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -27,11 +27,11 @@ public class HelloWorld {
         return result.toString();
     }
 
-    private void sayHello(String greeting) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird! "
+    private void sayHello(String salute) {
+        System.out.println("Hello, Mate! I am  happy to see you! This class looks weird! "
                 + "Your task is to fix the checkstyle in this file. "
                 + "Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(greeting);
+        System.out.println(salute);
     }
 
     private void initializeVariables() {
